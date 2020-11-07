4739b9464e40c59da6ca2edd4be79605b95d0f8d
frangf <franexyt@gmail.com>
2020-11-07 04:40:39 +0100

    Ejercicios

---
+++ Matrixes_PHP_exercises_2_Assoc.php
@@ -0,0 +1,32 @@
+<?php
+
+$Dias[0]="1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29.";
+$Dias[1]="1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30.";
+$Dias[2]="1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31.";
+
+$Meses[]="January: ";
+$Meses[]="February: ";
+$Meses[]="March: ";
+$Meses[]="April: ";
+$Meses[]="May: ";
+$Meses[]="June: ";
+$Meses[]="July: ";
+$Meses[]="August: ";
+$Meses[]="September: ";
+$Meses[]="October: ";
+$Meses[]="November: ";
+$Meses[]="December: ";
+
+echo  $Meses[0]," " .$Dias[2];
+echo  "<br>".$Meses[1], $Dias[0];
+echo  "<br>".$Meses[2], $Dias[2];
+echo  "<br>".$Meses[3], $Dias[1];
+echo  "<br>".$Meses[4], $Dias[2];
+echo  "<br>".$Meses[5], $Dias[1];
+echo  "<br>".$Meses[6], $Dias[2];
+echo  "<br>".$Meses[7], $Dias[2];
+echo  "<br>".$Meses[8], $Dias[1];
+echo  "<br>".$Meses[9], $Dias[2];
+echo  "<br>".$Meses[10], $Dias[1];
+echo  "<br>".$Meses[11], $Dias[2];
+?>
