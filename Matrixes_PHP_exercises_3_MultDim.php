945386afac38f1c62b7820314413459aabcec115
Violeta <violetaisusi@gmail.com>
2020-11-20 15:17:50 +0100

    matrixes 3 multiD - visusi corrections

--- Matrixes_PHP_exercises_3_MultDim.php
+++ Matrixes_PHP_exercises_3_MultDim.php
@@ -10,4 +10,10 @@ $Restaurant = array
 echo $Restaurant[0][0],$Restaurant[0][1],$Restaurant[0][2],$Restaurant[0][3],$Restaurant[0][4];
 echo $Restaurant[1][0],$Restaurant[1][1],$Restaurant[1][2],$Restaurant[1][3],$Restaurant[1][4],$Restaurant[1][5];
 echo $Restaurant[2][0],$Restaurant[2][1],$Restaurant[2][2],$Restaurant[2][3],$Restaurant[2][4],$Restaurant[2][5];
+//visusi correction // Els arrays multimiensionals també s'han de recorrer amb un bucle foreach com el que tens a continuació
+foreach ($Restaurant as $var1) {
+  foreach ($var1 as $valor1) {
+    echo $valor1;
+    }
+}
 ?>
