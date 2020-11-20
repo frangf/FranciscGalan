edac8dae6efa5f1dee82838aa4b7fd1ec7150b3a
Violeta <violetaisusi@gmail.com>
2020-11-20 15:52:35 +0100

    vectors to learn 2 - visusi corrections

--- CalendarioPHP.php
+++ CalendarioPHP.php
@@ -1,6 +1,15 @@
 <?php
+   //visusi - corrections - Reemplaçar l'string de numeros i comes per un array de numeros tal que: array(1,2,3,4,5,6.....31)
+   //Definir 3 varialbles que emmagatzement els arrays de dies tal que:
+   $array_31=array(1,2,......31);
+   $array_30=array(1,2,....30);
+   $array_29=array(1,2,....29);
+   //i emprar aquestes variables per a no repetir tant de codi igual
+   //Si hi ha dubtes, preguntar-les a classe
+
 #Creacion del vector
 $Calendario=array(
+   
     "January" => "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31<br>",
 
     "February" => "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29<br>",
